501b69b396fc3cfcfde74d501582dd2bee4adc7d
Zbigniew Siciarz <antyqjon@gmail.com>
2010-10-13 18:45:35 +0200

    Registered the Document model in Django admin site.

--- briefcase/admin.py
+++ briefcase/admin.py
@@ -8,4 +8,10 @@ class DocumentStatusAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'status', 'added_by', 'added_at', 'updated_at',)
+    list_display_links = ('__unicode__',)
+
+
 admin.site.register(DocumentStatus, DocumentStatusAdmin)
+admin.site.register(Document, DocumentAdmin)
