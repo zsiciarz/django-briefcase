5817cb3749059fa4c6def908417f5c423055fdfa
Zbigniew Siciarz <antyqjon@gmail.com>
2010-10-13 19:48:15 +0200

    Added a DocumentType model along with some migrations (phew...)

---
+++ briefcase/migrations/0006_auto__chg_field_document_type.py
@@ -0,0 +1,83 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Document.type'
+        db.alter_column('briefcase_document', 'type_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['briefcase.DocumentType']))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'Document.type'
+        db.alter_column('briefcase_document', 'type_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['briefcase.DocumentType'], null=True))
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'briefcase.document': {
+            'Meta': {'ordering': "['-added_at']", 'object_name': 'Document'},
+            'added_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'added_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['briefcase.DocumentStatus']", 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['briefcase.DocumentType']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'briefcase.documentstatus': {
+            'Meta': {'ordering': "['-name']", 'object_name': 'DocumentStatus'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '150', 'db_index': 'True'})
+        },
+        'briefcase.documenttype': {
+            'Meta': {'ordering': "['name']", 'object_name': 'DocumentType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mimetype': ('django.db.models.fields.CharField', [], {'default': "'application/octet-stream'", 'max_length': '127'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['briefcase']
--- briefcase/models.py
+++ briefcase/models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import mimetypes
 import os
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.fields.files import FieldFile
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -26,13 +28,51 @@ class DocumentStatus(models.Model):
         return self.name
 
 
+class DocumentType(models.Model):
+    u"""
+    File type (eg. PDF file, MS Word file etc.).
+    """
+    mimetype    = models.CharField(_("MIME type"), max_length=127, default="application/octet-stream", help_text=_("File MIME type as defined by <a href=\"http://tools.ietf.org/html/rfc4288#section-4.2\">RFC 4288</a>, for example: 'image/jpeg'"))
+    name        = models.CharField(_("Full name"), max_length=250, blank=True, help_text=_("Human-readable name of the type, for example 'JPG Image'"))
+
+    class Meta:
+            verbose_name = _("Document type")
+            verbose_name_plural = _("Document types")
+            ordering = ['name']
+
+    def __unicode__(self):
+        if self.name:
+            return self.name
+        return self.mimetype
+
+    @classmethod
+    def unknown_type(cls):
+        u"""
+        Returns a default document type - a generic "Unknown type".
+        
+        This provides a sensible default value for a Document before saving it.
+        """
+        return cls.objects.get_or_create(mimetype="application/octet-stream",
+                                         name="Unknown type")
+    
+    @classmethod
+    def type_for_file(cls, file):
+        if isinstance(file, FieldFile):
+            filename = file.name
+        else:
+            filename = file
+        mimetype, encoding = mimetypes.guess_type(filename)
+        mimetype = mimetype or "application/octet-stream"
+        return cls.objects.get_or_create(mimetype=mimetype, name=mimetype)
+
+
 class Document(models.Model):
     u"""
     The document itself.
     """
     # Basic document data.
     file = models.FileField(verbose_name=_("file"), upload_to='uploads/%Y/%m/%d/', help_text=_("This is the document itself - well, just a file."))
-
+    type = models.ForeignKey(DocumentType, verbose_name=_("document type"), default=DocumentType.unknown_type, help_text=_("Document type, for example 'Microsoft Word Document' or 'PDF File'."))
     # Meta-information.
     status      = models.ForeignKey(DocumentStatus, verbose_name=_("document status"), null=True, blank=True)
     added_by    = models.ForeignKey(User, verbose_name=_("added by"), null=True, blank=True, editable=False)
