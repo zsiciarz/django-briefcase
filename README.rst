7e6552b9c25e508a20d679992a1242d28c4b718e
Zbigniew Siciarz <antyqjon@gmail.com>
2010-10-13 17:30:53 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,17 @@
+===============
+django-briefcase
+===============
+
+``django-briefcase`` is yet another document management app for Django.
+
+
+License
+=======
+django-briefcase is free software, licensed under the MIT/X11 License. A copy of
+the license is provided with the application in the LICENSE file.
+
+
+Author
+======
+
+Zbigniew Siciarz (antyqjon atty gmail dotty com)
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-briefcase',
+    version=__import__('briefcase').__version__,
+    description='Yet another document management app for Django.',
+    long_description=read('README.rst'),
+    author='Zbigniew Siciarz',
+    author_email='antyqjon@gmail.com',
+    url='http://github.com/zsiciarz/django-briefcase',
+    license='MIT',
+    install_requires=['Django', 'south'],
+    packages=[
+        'briefcase',
+    ],
+    include_package_data=True,
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)
