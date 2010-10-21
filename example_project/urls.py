ebf51b9e41dbd536e034819996c0b25720637949
Zbigniew Siciarz <antyqjon@gmail.com>
2010-10-21 21:40:37 +0200

    Introduced the download view in the example project.

--- example_project/urls.py
+++ example_project/urls.py
@@ -17,10 +17,11 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('briefcase.views',
-    url(r'^$',                                  'general.recent_documents', name='recent_documents'),
-    url(r'^type/(?P<extension>\w+)/$',          'general.recent_documents', name='recent_documents_by_extension'),
-    url(r'^my_documents/$',                     'per_user.my_documents',    name='my_documents'),
-    url(r'^my_documents/(?P<extension>\w+)/$',  'per_user.my_documents',    name='my_documents_by_extension'),
+    url(r'^$',                                  'general.recent_documents',     name='recent_documents'),
+    url(r'^type/(?P<extension>\w+)/$',          'general.recent_documents',     name='recent_documents_by_extension'),
+    url(r'^my_documents/$',                     'per_user.my_documents',        name='my_documents'),
+    url(r'^my_documents/(?P<extension>\w+)/$',  'per_user.my_documents',        name='my_documents_by_extension'),
+    url(r'^download/(?P<document_id>\d+)/$',    'general.download_document',    name='download_document')
 )
 
 if settings.DEBUG:
