db53a4d52d57aa1958faa3e7cfdd03f990fa1edc
Zbigniew Siciarz <antyqjon@gmail.com>
2010-10-27 15:13:23 +0200

    Problem with data file seems to be fixed.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     license='MIT',
     install_requires=['Django', 'south'],
     packages=find_packages(exclude=['example_project']),
-    #package_data = {'briefcase': ['data/mime.types']},
     include_package_data=True,
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Web Environment',
